7464e8497d75cc2ab42f3d8d17b8cc38f3da12fd
Mauricio Reyes <mreyes@talpor.com>
2019-08-14 01:12:20 -0400

    add deploy script

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric.api import env, run, cd
+import time
+
+env.hosts = ["wololo@wololo.appjango.com"]
+env.key_filename = "~/.ssh/id_rsa.pub"
+env.forward_agent = True
+
+
+def deploy():
+    with cd("~/wololo-tournaments-api"):
+        run("git pull")
+        run("docker-compose -f production.yml down")
+        # waiting for the release of RAM
+        time.sleep(15)
+        run("docker-compose -f production.yml build")
+        run("docker-compose -f production.yml up -d")
